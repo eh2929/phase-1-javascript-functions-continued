177402ab1e0df0fb0223ba5abc64a38fb4d285fb
Ethan <ethanhough29@gmail.com>
2023-12-18 19:19:12 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activityDefault = "roller-skate") {
+  return `This Saturday, I want to ${activityDefault}!`;
+}
+console.log(saturdayFun());
+
+function mondayWork(mondayDefault = "go to the office") {
+  return `This Monday, I will ${mondayDefault}.`;
+}
+console.log(mondayWork());
+
+function wrapAdjective(flair = "*") {
+  const innerFunction = function (adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+  return innerFunction;
+}
+console.log(wrapAdjective()());
